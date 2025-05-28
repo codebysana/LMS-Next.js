22f301c4baf037982fa77fc01b67507ff417cb55
codebysana <sana.younis05@gmail.com>
2025-05-29 01:02:56 +0500

    Work Complete

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React, {FC, useState} from "react";
 import Heading from "./utils/Heading";
+import Header from "./components/Header.tsx";
 
 interface Props{
 
@@ -11,6 +12,7 @@ const Page: FC<Props> = (props) => {
     <div>
       <Heading title="LMS" description="LMS is a platform where students can enroll and learn courses from international teachers."
       keywords="Programming, MERN, Redux Toolkit, Machine Lerning"/>
+      <Header/>
     </div>
   )
 };
