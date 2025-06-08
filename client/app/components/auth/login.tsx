9dab52af34f0f92a8ed949573bb172e2e867a098
codebysana <sana.younis05@gmail.com>
2025-06-08 23:58:16 +0500

    Work Complete

--- client/app/components/auth/login.tsx
+++ client/app/components/auth/login.tsx
@@ -1,15 +1,27 @@
-import React, { FC } from "react";
+"use client";
+import React, { FC, useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import { FcGooogle } from "react-icons/fc";
 import {
   AiOutlineEye,
   AiOutlineEyeInvisible,
   AiFillGithub,
 } from "react-icons/ai";
 
-type Props = {};
+type Props = {
+  setRoute: (route: string) => void;
+};
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email("Invalid Email!")
+    .required("Please enter your email"),
+  password: Yup.string().required("Please enter your password!").min(6),
+});
 
 const login: FC<Props> = (props: Props) => {
+  const [show, setShow] = useState(false);
   return <div></div>;
 };
 
