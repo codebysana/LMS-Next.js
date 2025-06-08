de7f26f7164f7d9b7d836d901bf8fc05db603bd7
codebysana <sana.younis05@gmail.com>
2025-06-09 00:44:58 +0500

    Work Complete

--- client/app/components/auth/login.tsx
+++ client/app/components/auth/login.tsx
@@ -8,6 +8,7 @@ import {
   AiOutlineEyeInvisible,
   AiFillGithub,
 } from "react-icons/ai";
+import { styles } from "../../styles/style";
 
 type Props = {
   setRoute: (route: string) => void;
@@ -22,10 +23,34 @@ const schema = Yup.object().shape({
 
 const login: FC<Props> = (props: Props) => {
   const [show, setShow] = useState(false);
+
   const formik = useFormik({
-    initialValues: { email: "", password = "" },
+    initialValues: { email: "", password: "" },
+    validationSchema: schema,
+    onSubmit: async ({ email, password }) => {
+      console.log(email, password);
+    },
   });
-  return <div></div>;
+  const { errors, touched, values, handleChange, handleSubmit } = formik;
+  return (
+    <div className="w-full">
+      <h1 className={`${styles.title}`}>Login with LMS</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email" className={`${styles.label}`}></label>
+        <input
+          id="email"
+          type="email"
+          placeholder="example@gmail.com"
+          name=""
+          value={values.email}
+          onChange={handleChange}
+          className={`${
+            errors.email && touched.email && "border-red-500"
+          } w-full text-black dark:text-white bg-transparent border rounded h-[40px] px-2 outline-none mt-[10px] font-Poppins`}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default login;
