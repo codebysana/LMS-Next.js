9c76a988e79c13b79dd8d93d05b85b8f922944e7
codebysana <sana.younis05@gmail.com>
2025-06-09 02:58:15 +0500

    Work Complete

--- client/app/components/auth/login.tsx
+++ client/app/components/auth/login.tsx
@@ -2,7 +2,7 @@
 import React, { FC, useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import { FcGooogle } from "react-icons/fc";
+import { FcGoogle } from "react-icons/fc";
 import {
   AiOutlineEye,
   AiOutlineEyeInvisible,
@@ -21,7 +21,7 @@ const schema = Yup.object().shape({
   password: Yup.string().required("Please enter your password!").min(6),
 });
 
-const login: FC<Props> = (props: Props) => {
+const login: FC<Props> = ({ setRoute }) => {
   const [show, setShow] = useState(false);
 
   const formik = useFormik({
@@ -81,7 +81,28 @@ const login: FC<Props> = (props: Props) => {
               onClick={() => setShow(false)}
             />
           )}
+          {errors.email && touched.email && (
+            <span className="text-red-500 pt-2 block">{errors.email}</span>
+          )}
         </div>
+        <div className="w-full mt-5">
+          <input type="submit" value="Login" className={`${styles.button}`} />
+        </div>
+        <br />
+        <h5 className="text-center pt-4 font-Poppins text-[14px] text-black dark:text-white">
+          Or join with
+        </h5>
+        <div className="flex items-center justify-center my-3">
+          <FcGoogle size={30} className="cursor-pointer mr-2" />
+          <AiFillGithub size={30} className="cursor-pointer ml-2" />
+        </div>
+        <h5 className="text-center pt-4 font-Poppins text-[14px]">
+          Have not any account?
+          <span
+            className="text-[#2190ff] pl-1 cursor-pointer"
+            onClick={() => setRoute("Sign-Up")}
+          ></span>
+        </h5>
       </form>
     </div>
   );
