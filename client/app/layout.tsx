389b3360603d69721429f58a841bc3cad8c9570d
codebysana <sana.younis05@gmail.com>
2025-06-11 17:58:53 +0500

    Work Complete

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from "react-hot-toast";
 import { Providers } from "./provider";
 import { SessionProvider } from "next-auth/react";
 import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
-import Loader from "./components/loader/Loader"
+import Loader from "./components/loader/Loader";
 
 const poppins = Poppins({
   subsets: ["latin"],
