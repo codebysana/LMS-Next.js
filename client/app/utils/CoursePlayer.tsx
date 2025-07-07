3071f3f917ac9fe98db341587c43310fbb450de9
codebysana <sana.younis05@gmail.com>
2025-07-07 10:40:53 +0500

    Work Complete

--- client/app/utils/CoursePlayer.tsx
+++ client/app/utils/CoursePlayer.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
+import axios from "axios";
 
 type Props = {
   videoUrl: string;
@@ -6,7 +7,39 @@ type Props = {
 };
 
 const CoursePlayer: FC<Props> = ({ title, videoUrl }) => {
-  return <div></div>;
+  const [videoData, setVideoData] = useState({
+    otp: "",
+    playbackInfo: "",
+  });
+
+  useEffect(() => {
+    axios
+      .post(`${process.env.NEXT_PUBLIC_API_URL}getVdoCipherOTP`, {
+        videoId: videoUrl,
+      })
+      .then((res) => {
+        setVideoData(res.data);
+      });
+  }, [videoUrl]);
+  return (
+    <div style={{ paddingTop: "41%", position: "relative" }}>
+      {videoData.otp && videoData.playbackInfo !== "" && (
+        <iframe
+          src={`https://player.vdocipher.com/v2/?otp=${videoData?.otp}&playbackInfo=${videoData.playbackInfo}&player=Ub90iZXI0eUXH0Nv`}
+          style={{
+            border: 0,
+            width: "90%",
+            height: "100%",
+            position: "absolute",
+            top: 0,
+            left: 0,
+          }}
+          allowFullScreen={true}
+          allow="encrypted-media"
+        ></iframe>
+      )}
+    </div>
+  );
 };
 
 export default CoursePlayer;
