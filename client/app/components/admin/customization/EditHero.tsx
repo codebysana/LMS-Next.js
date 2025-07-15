496f884b19629fa6bab3561c3dc272167aa5a669
codebysana <sana.younis05@gmail.com>
2025-07-16 04:26:11 +0500

    Work Complete

--- client/app/components/admin/customization/EditHero.tsx
+++ client/app/components/admin/customization/EditHero.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @next/next/no-img-element */
 import { useGetHeroDataQuery } from "@/redux/features/layout/layoutApi";
 import React, { FC, useEffect, useState } from "react";
+import { AiOutlineCamera } from "react-icons/ai";
 
 const EditHero: FC<Props> = (props: Props) => {
   const [image, setImage] = useState("");
@@ -17,11 +19,32 @@ const EditHero: FC<Props> = (props: Props) => {
     }
   }, []);
 
+  const handleUpdateImage = (e:any) => {
+
+  };
+
   return (
     <>
       <div className="w-full 1000px:flex items-center">
         <div className="absolute top-[100px] 1000px:top-[unset] 1500px:h-[700px] 1500px:w-[700px] 1100px:h-[500px] 1100px:w-[500px] h-[50vh] w-[50vh] hero-animation rounded-[50%] 1100px:left-[18rem] 1500px:left-[21rem]">
-
+          <div className="1000px:w-[40%] flex 1000px:min-h-screen items-center justify-end pt-[70px] 1000px:pt-[0] z-10">
+            <img
+              src={image}
+              alt=""
+              className="object-contain 1100px:max-w-[90%] w-[90%] 1500px:max-w-[85%] h-[auto] z-[10]"
+            />
+            <input
+              type="file"
+              name=""
+              id="banner"
+              accept="image/*"
+              onChange={handleUpdateImage}
+              className="hidden"
+            />
+            <label htmlFor="banner" className="ABSOLUTE bottom-0 right-0 z-20">
+              <AiOutlineCamera className="dark:text-white text-black text-[18px] cursor-pointer" />
+            </label>
+          </div>
         </div>
       </div>
     </>
