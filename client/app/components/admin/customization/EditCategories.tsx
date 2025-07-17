6d7a3357466aed93ba03f2cdfaf7e6b7698d649b
codebysana <sana.younis05@gmail.com>
2025-07-18 03:32:54 +0500

    Work Complete

--- client/app/components/admin/customization/EditCategories.tsx
+++ client/app/components/admin/customization/EditCategories.tsx
@@ -6,9 +6,11 @@ import React, { useEffect, useState } from "react";
 import Loader from "../../loader/Loader";
 import { styles } from "@/app/styles/style";
 import { AiOutlineDelete } from "react-icons/ai";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import toast from "react-hot-toast";
 
 const EditCategories = () => {
-  const { data, isLoading } = useGetHeroDataQuery("Categories", {
+  const { data, isLoading, refetch } = useGetHeroDataQuery("Categories", {
     refetchOnMountOrArgChange: true,
   });
 
@@ -20,13 +22,55 @@ const EditCategories = () => {
     if (data) {
       setCategories(data.layout.categories);
     }
-  }, [data]);
+    if (layoutSuccess) {
+      refetch();
+      toast.success("Categories updated successfully");
+    }
+    if (error) {
+      if ("data" in error) {
+        const errorData = error as any;
+        toast.error(errorData?.data?.message);
+      }
+    }
+  }, [data, layoutSuccess, error]);
 
   const handleCategoriesAdd = (id: any, value: string) => {
     setCategories((prevCategory: any) =>
       prevCategory.map((i: any) => (i._id === id ? { ...i, title: value } : i))
     );
   };
+
+  const newCategoriesHandler = () => {
+    if (categories[categories.length - 1].title === "") {
+      toast.error("Category title can't be empty");
+    } else {
+      setCategories((prevCategory: any) => [...prevCategory, { title: "" }]);
+    }
+  };
+
+  const areCategoriesUnchanged = (
+    originalCategories: any[],
+    newCategories: any[]
+  ) => {
+    return JSON.stringify(originalCategories) === JSON.stringify(newCategories);
+  };
+
+  const isAnyCategoryTitleEmpty = (categories: any[]) => {
+    return categories.some((q) => q.title === "");
+  };
+
+  const editCategoriesHandler = async () => {
+    if (
+      !areCategoriesUnchanged(data.layout.categories, categories) &&
+      !isAnyCategoryTitleEmpty(categories)
+    ) {
+      await editLayout({
+        type: "Categories",
+        categories,
+      });
+    }
+  };
+
   return (
     <>
       {isLoading ? (
@@ -58,6 +102,32 @@ const EditCategories = () => {
                 </div>
               );
             })}
+          <br />
+          <br />
+          <div className="w-full flex justify-between">
+            <IoMdAddCircleOutline
+              className="dark:text-white text-black text-[25px] cursor-pointer"
+              onClick={newCategoriesHandler}
+            />
+          </div>
+          <div
+            className={`${
+              styles.button
+            } !w-[100px] !min-h-[40px] !h-[40px] dark:text-white text-black bg-[#cccccc34] ${
+              areCategoriesUnchanged(data.layout.categories, categories) ||
+              isAnyCategoryTitleEmpty(categories)
+                ? "!cursor-not-allowed"
+                : "!cursor-pointer !bg-[#42d383]"
+            } !rounded absolute bottom-12 right-12`}
+            onClick={
+              areCategoriesUnchanged(data.layout.categories, categories) ||
+              isAnyCategoryTitleEmpty(categories)
+                ? () => null
+                : editCategoriesHandler
+            }
+          >
+            Save
+          </div>
         </div>
       )}
     </>
