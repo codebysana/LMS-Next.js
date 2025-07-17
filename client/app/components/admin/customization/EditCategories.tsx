df8ab90f916ed2d21975ac61c5a80d707a261e00
codebysana <sana.younis05@gmail.com>
2025-07-18 01:40:47 +0500

    Work Complete

--- client/app/components/admin/customization/EditCategories.tsx
+++ client/app/components/admin/customization/EditCategories.tsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import {
+  useEditLayoutMutation,
+  useGetHeroDataQuery,
+} from "@/redux/features/layout/layoutApi";
+import React, { useEffect, useState } from "react";
+import Loader from "../../loader/Loader";
+import { styles } from "@/app/styles/style";
+import { AiOutlineDelete } from "react-icons/ai";
 
 const EditCategories = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { data, isLoading } = useGetHeroDataQuery("Categories", {
+    refetchOnMountOrArgChange: true,
+  });
 
-export default EditCategories
+  const [editLayout, { isSuccess: layoutSuccess, error }] =
+    useEditLayoutMutation();
+  const [categories, setCategories] = useState<any>([]);
+
+  useEffect(() => {
+    if (data) {
+      setCategories(data.layout.categories);
+    }
+  }, [data]);
+
+  const handleCategoriesAdd = (id: any, value: string) => {
+    setCategories((prevCategory: any) =>
+      prevCategory.map((i: any) => (i._id === id ? { ...i, title: value } : i))
+    );
+  };
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="mt-[120px] text-center">
+          <h1 className={`${styles.title}`}>All Categories</h1>
+          {categories &&
+            categories.map((item: any, index: number) => {
+              return (
+                <div className="p-3" key={index}>
+                  <div className="flex items-center justify-center w-full">
+                    <input
+                      className={`${styles.input} !w-[unset] !border-none !text-[20px]`}
+                      value={item.title}
+                      onChange={(e) =>
+                        handleCategoriesAdd(item._id, e.target.value)
+                      }
+                    />
+                    <AiOutlineDelete
+                      className="dark:text-white text-black text-[18px] cursor-pointer"
+                      onClick={() => {
+                        setCategories((prevCategory: any) =>
+                          prevCategory.filter((i: any) => i._id !== item._id)
+                        );
+                      }}
+                    />
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default EditCategories;
