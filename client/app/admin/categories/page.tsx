b1a54e48e55da5646b07cb3cb72e49080a663dce
codebysana <sana.younis05@gmail.com>
2025-07-18 01:07:57 +0500

    Work Complete

---
+++ client/app/admin/categories/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React from "react";
+import Heading from "@/app/utils/Heading";
+import DashboardHero from "@/app/components/admin/DashboardHero";
+import AdminSidebar from "@/app/components/admin/sidebar/AdminSidebar";
+import AdminProtected from "@/app/hooks/adminProtected";
+import EditCategories from "@/app/components/admin/customization/EditCategories";
+
+type Props = {};
+
+const page = () => {
+  return (
+    <div>
+      <AdminProtected>
+        <Heading
+          title="ScholarNet - Admin"
+          description="ScholarNet is a platform for students to learn and get help from community."
+          keywords="Programming, Mern, Redux, Machine Learning"
+        />
+        <div className="flex h-screen">
+          <div className="1500px:w-[16%] w-1/5">
+            <AdminSidebar />
+          </div>
+          <div className="w-[85%]">
+            <DashboardHero />
+            <EditCategories />
+          </div>
+        </div>
+      </AdminProtected>
+    </div>
+  );
+};
+
+export default page;
