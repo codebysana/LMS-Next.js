2fbc7645588351ca2cc7f08bbab0446b180a99cb
codebysana <sana.younis05@gmail.com>
2025-07-22 05:33:07 +0500

    Work Complete

---
+++ client/app/admin/invoices/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+import Heading from "../../../app/utils/Heading";
+import AdminSidebar from "../../components/admin/sidebar/AdminSidebar";
+import AllInvoices from "@/app/components/admin/order/AllInvoices";
+
+type Props = {};
+
+const page = ({}: Props) => {
+  return (
+    <div>
+      <Heading
+        title="ScholarNet - Admin"
+        description="ScholarNet is a platform for students to learn and get help from community."
+        keywords="Programming, Mern, Redux, Machine Learning"
+      />
+      <div className="flex h-[200vh]">
+        <div className="1500px:w-[16%] w-1/5">
+          <AdminSidebar />
+        </div>
+        <div className="w-[85%]">
+          <AllInvoices />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- client/app/components/admin/order/AllInvoices.tsx
+++ client/app/components/admin/order/AllInvoices.tsx
@@ -1,7 +1,174 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { Box } from "@mui/material";
+import { useTheme } from "next-themes";
+import { useGetAllCoursesQuery } from "@/redux/features/courses/coursesApi";
+import Loader from "../../loader/Loader";
+import { format } from "timeago.js";
+import { useGetAllOrdersQuery } from "@/redux/features/orders/ordersApi";
+import { useGetAllUsersQuery } from "@/redux/features/user/userApi";
+import { AiOutlineMail } from "react-icons/ai";
+import { useEffect } from "react";
+import CourseData from "../courses/CourseData";
+
 type Props = {
   isDashboard?: boolean;
 };
 
-const AllInvoices = ({ isDashboard }: Props) => {};
+const AllInvoices = ({ isDashboard }: Props) => {
+  const { theme, setTheme } = useTheme();
+  const { isLoading, data } = useGetAllOrdersQuery({});
+  const { data: userData } = useGetAllUsersQuery({});
+  const { data: coursesData } = useGetAllCoursesQuery({});
+
+  const [orderData, setOrderData] = useState<any>([]);
+
+  useEffect(() => {
+    if (data) {
+      const temp = data.orders.mao((item: any) => {
+        const user = usersData?.users.find(
+          (user: any) => user._id === item.userId
+        );
+        const course = CourseData?.courses.find(
+          (course: any) => course._id === item.courseId
+        );
+        return {
+          ...item,
+          userName: user?.name,
+          userEmail: user?.email,
+          title: course?.name,
+          price: "$" + course?.price,
+        };
+      });
+      setOrderData(temp);
+    }
+  }, [data, userData, coursesData]);
+
+  const columns: any = [
+    { field: "id", headerName: "ID", flex: 0.3 },
+    { field: "username", headerName: "Name", flex: isDashboard ? 0.6 : 0.5 },
+    ...(isDashboard
+      ? []
+      : [
+          { field: "userEmail", headerName: "Email", flex: 1 },
+          { field: "title", headerName: "Course Title", flex: 1 },
+        ]),
+    { field: "price", headerName: "Price", flex: 0.5 },
+    ...(isDashboard
+      ? [{ field: "created_at", headerName: "Created At", flex: 0.5 }]
+      : [
+          {
+            field: " ",
+            headerName: "Email",
+            flex: 0.2,
+            renderCell: (params: any) => {
+              return (
+                <a href={`mailto:${params.row.userEmail}`}>
+                  <AiOutlineMail
+                    className="dark:text-white text-black"
+                    size={20}
+                  />
+                </a>
+              );
+            },
+          },
+        ]),
+  ];
+
+  const rows: any = [
+    {
+      id: "1234567898767",
+      userName: "Sana",
+      userEmail: "sana.younis05@gmail.com",
+      title: "React Js Course",
+      price: "$500",
+      created_at: "2 days ago",
+    },
+  ];
+
+  orderData &&
+    orderData.forEach((item: any) => {
+      rows.push({
+        id: item._id,
+        userName: item.userName,
+        userEmail: item.userEmail,
+        title: item.title,
+        price: item.price,
+        created_at: format(item.createdAt),
+      });
+    });
+
+  return (
+    <div className={!isDashboard ? "mt-[120px]" : "mt-[0px]"}>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Box m={isDashboard ? "0" : "40px"}>
+          <Box
+            m={isDashboard ? "0" : "40px 0 0 0"}
+            height={isDashboard ? "35vh" : "90vh"}
+            overflow={"hidden"}
+            sx={{
+              "& .MuiDataGrid-root": {
+                border: "none",
+                outline: "none",
+              },
+              "& .css-pqjvzy-MuiSvgIcon-root-MuiSelect-icon": {
+                color: theme === "dark" ? "#fff" : "#000",
+              },
+              "& .MuiDataGrid-sortIcon": {
+                color: theme === "dark" ? "#fff" : "#000",
+              },
+              "& .MuiDataGrid-row": {
+                color: theme === "dark" ? "#fff" : "#000",
+                borderBottom:
+                  theme === "dark"
+                    ? "1px solid #ffffff30 !important"
+                    : "1px solid #ccc !important",
+              },
+              "& .MuiTablePagination-root": {
+                color: theme === "dark" ? "#fff" : "#000",
+              },
+              "& .MuiDataGrid-cell": {
+                borderBottom: "none !important",
+              },
+              "& .name-column--cell": {
+                color: theme === "dark" ? "#fff" : "#000",
+              },
+              "& .MuiDataGrid-columnHeaders": {
+                backgroundColor: theme === "dark" ? "#3e4396" : "#a4a9fc",
+                borderBottom: "none",
+                color: theme === "dark" ? "#fff" : "#000",
+              },
+              "& .MuiDataGrid-virtualScroller": {
+                backgroundColor: theme === "dark" ? "#1f2a40" : "#f2f0f0",
+              },
+              "& .MuiDataGrid-footerContainer": {
+                color: theme === "dark" ? "#fff" : "#000",
+                borderBottom: "none",
+                backgroundColor: theme === "dark" ? "#3e4396" : "#a4a9fc",
+              },
+              "& .MuiCheckBox-root": {
+                color:
+                  theme === "dark" ? "#b7ebde !important" : "#000 !important",
+              },
+              "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+                color: "#fff !important",
+              },
+            }}
+          >
+            <DataGrid
+              checkboxSelection={isDashboard ? false : true}
+              rows={rows}
+              columns={columns}
+              components={isDashboard ? {} : { Toolbar: GridToolbar }}
+            />
+          </Box>
+        </Box>
+      )}
+    </div>
+  );
+};
 
 export default AllInvoices;
